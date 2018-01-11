f0161d600a51463e48465a2448ea89546c10f903
Nadav Bhonker <nadavbh@gmail.com>
2018-01-11 14:54:25 +0200

    added unit test for nearest_embed

---
+++ test_nearest_embed.py
@@ -0,0 +1,97 @@
+import unittest
+
+import numpy as np
+import torch
+from torch import nn
+from torch.autograd import Variable
+
+from nearest_embed import nearest_embed
+
+# class NearestEmbedTest(unittest.TestCase):
+#     def test_something(self):
+#
+#         emb = Variable(torch.eye(10, 10).double())
+#         a = np.array(([1,0,0,0,0,0,0,0,0,0],
+#                       [0,1,0,0,0,0,0,0,0,0]), dtype=np.double)
+#         input = Variable(torch.from_numpy(a))
+#         z_q = nearest_embed(input, emb, dim=1)
+#         self.assertEqual(True, torch.equal(z_q.data, input.data))
+
+
+class NearestEmbed2dTest(unittest.TestCase):
+    def test_something(self):
+
+        # inputs
+        emb = Variable(torch.eye(5, 5).double(), requires_grad=True)
+        a = np.array(([[[0.9, 0.0, 0.0, 0.0, 0.0],
+                        [0.0, 0.8, 0.0, 0.0, 0.0]],
+                       [[0.0, 0.0, 0.7, 0.0, 0.0],
+                        [0.0, 0.0, 0.0, 0.6, 0.0]]]),dtype=np.double).reshape((2,5,2))
+
+        # expected results
+        result = np.array(([[[1., 0., 0., 0., 0.],
+                             [0., 1., 0., 0., 0.]],
+                            [[0., 0., 1., 0., 0.],
+                             [0., 0., 0., 1., 0.]]]), dtype=np.double).reshape((2,5,2))
+        grad_input = np.array(([[[1., 0., 0., 0., 0.],
+                                 [0., 1., 0., 0., 0.]],
+                                [[0., 0., 1., 0., 0.],
+                                 [0., 0., 0., 1., 0.]]]), dtype=np.double).reshape((2,5,2))
+        grad_emb = np.array(([[1., 0., 0., 0., 0.],
+                              [0., 1., 0., 0., 0.],
+                              [0., 0., 1., 0., 0.],
+                              [0., 0., 0., 1., 0.],
+                              [0., 0., 0., 0., 0.]]), dtype=np.double)
+
+        grad_input = torch.from_numpy(grad_input).double()
+        grad_emb = torch.from_numpy(grad_emb).double()
+
+        input = Variable(torch.from_numpy(a).double(), requires_grad=True)
+        z_q = nearest_embed(input, emb)
+
+        (0.5*z_q.pow(2)).sum().backward(retain_graph=True)
+        result = torch.from_numpy(result)
+
+        self.assertEqual(True, torch.equal(z_q.data, result))
+        self.assertEqual(True, torch.equal(input.grad.data, grad_input))
+        self.assertEqual(True, torch.equal(emb.grad.data, grad_emb))
+
+    def test_multiple_same_embedding(self):
+        # inputs
+        emb = Variable(torch.eye(5, 5).double(), requires_grad=True)
+        a = np.array(([[[0.9, 0.0, 0.0, 0.0, 0.0],
+                        [0.0, 0.8, 0.0, 0.0, 0.0]],
+                       [[0.0, 0.7, 0.0, 0.0, 0.0],
+                        [0.0, 0.6, 0.0, 0.0, 0.0]]]), dtype=np.double).reshape((2,5,2))
+
+        # expected results
+        result = np.array(([[[1., 0., 0., 0., 0.],
+                             [0., 1., 0., 0., 0.]],
+                            [[0., 1., 0., 0., 0.],
+                             [0., 1., 0., 0., 0.]]]), dtype=np.double).reshape((2,5,2))
+        grad_input = np.array(([[[1., 0., 0., 0., 0.],
+                                 [0., 1., 0., 0., 0.]],
+                                [[0., 1., 0., 0., 0.],
+                                 [0., 1., 0., 0., 0.]]]), dtype=np.double).reshape((2,5,2))
+        grad_emb = np.array(([[1., 0., 0., 0., 0.],
+                              [0., 1., 0., 0., 0.],
+                              [0., 0., 0., 0., 0.],
+                              [0., 0., 0., 0., 0.],
+                              [0., 0., 0., 0., 0.]]), dtype=np.double)
+
+        grad_input = torch.from_numpy(grad_input).double()
+        grad_emb = torch.from_numpy(grad_emb).double()
+
+        input = Variable(torch.from_numpy(a).double(), requires_grad=True)
+        z_q = nearest_embed(input, emb)
+
+        (0.5 * z_q.pow(2)).sum().backward(retain_graph=True)
+        result = torch.from_numpy(result)
+
+        self.assertEqual(True, torch.equal(z_q.data, result))
+        self.assertEqual(True, torch.equal(input.grad.data, grad_input))
+        self.assertEqual(True, torch.equal(emb.grad.data, grad_emb))
+
+
+if __name__ == '__main__':
+    unittest.main()
