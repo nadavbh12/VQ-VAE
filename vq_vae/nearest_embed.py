056b501a3946b6ae6ac849d11d44ad7a5f91f1a4
Nadavb <nadavb@amazon.com>
2019-10-02 15:08:13 +0300

    add nearest_embed_ema

--- vq_vae/nearest_embed.py
+++ vq_vae/nearest_embed.py
@@ -2,6 +2,7 @@ import numpy as np
 import torch
 from torch import nn
 from torch.autograd import Function, Variable
+import torch.nn.functional as F
 
 
 class NearestEmbedFunc(Function):
@@ -26,7 +27,7 @@ class NearestEmbedFunc(Function):
         ctx.input_type = type(input)
         ctx.dims = list(range(len(input.size())))
 
-        # expand so it broadcastable
+        # expand to be broadcast-able
         x_expanded = input.unsqueeze(-1)
         num_arbitrary_dims = len(ctx.dims) - 2
         if num_arbitrary_dims:
@@ -58,8 +59,8 @@ class NearestEmbedFunc(Function):
             idx_avg_choices = idx_choices / n_idx_choice
             grad_output = grad_output.permute(0, *ctx.dims[2:], 1).contiguous()
             grad_output = grad_output.view(ctx.batch_size * ctx.num_latents, ctx.emb_dim)
-            grad_emb = Variable(torch.sum(
-                grad_output.data.view(-1, ctx.emb_dim, 1) * idx_avg_choices.view(-1, 1, ctx.num_emb), 0))
+            grad_emb = torch.sum(grad_output.data.view(-1, ctx.emb_dim, 1) *
+                                 idx_avg_choices.view(-1, 1, ctx.num_emb), 0)
         return grad_input, grad_emb, None, None
 
 
@@ -78,3 +79,63 @@ class NearestEmbed(nn.Module):
         x - (batch_size, emb_size, *)
         """
         return nearest_embed(x, self.weight.detach() if weight_sg else self.weight)
+
+
+# adapted from https://github.com/rosinality/vq-vae-2-pytorch/blob/master/vqvae.py#L25
+# that adapted from https://github.com/deepmind/sonnet
+
+
+class NearestEmbedEMA(nn.Module):
+    def __init__(self, n_emb, emb_dim, decay=0.99, eps=1e-5):
+        super(NearestEmbedEMA, self).__init__()
+        self.decay = decay
+        self.eps = eps
+        self.embeddings_dim = emb_dim
+        self.n_emb = n_emb
+        self.emb_dim = emb_dim
+        embed = torch.rand(emb_dim, n_emb)
+        self.register_buffer('weight', embed)
+        self.register_buffer('cluster_size', torch.zeros(n_emb))
+        self.register_buffer('embed_avg', embed.clone())
+
+    def forward(self, x):
+        """Input:
+        ---------
+        x - (batch_size, emb_size, *)
+        """
+
+        dims = list(range(len(x.size())))
+        x_expanded = x.unsqueeze(-1)
+        num_arbitrary_dims = len(dims) - 2
+        if num_arbitrary_dims:
+            emb_expanded = self.weight.view(self.emb_dim, *([1] * num_arbitrary_dims), self.n_emb)
+        else:
+            emb_expanded = self.weight
+
+        # find nearest neighbors
+        dist = torch.norm(x_expanded - emb_expanded, 2, 1)
+        _, argmin = dist.min(-1)
+        shifted_shape = [x.shape[0], *list(x.shape[2:]), x.shape[1]]
+        result = self.weight.t().index_select(0, argmin.view(-1)).view(shifted_shape).permute(0, dims[-1], *dims[1:-1])
+
+        if self.training:
+            latent_indices = torch.arange(self.n_emb).type_as(argmin)
+            emb_onehot = (argmin.view(-1, 1) == latent_indices.view(1, -1)).type_as(x.data)
+            n_idx_choice = emb_onehot.sum(0)
+            n_idx_choice[n_idx_choice == 0] = 1
+            flatten = x.permute(1, 0, *dims[-2:]).contiguous().view(x.shape[1], -1)
+
+            self.cluster_size.data.mul_(self.decay).add_(
+                1 - self.decay, n_idx_choice
+            )
+            embed_sum = flatten @ emb_onehot
+            self.embed_avg.data.mul_(self.decay).add_(1 - self.decay, embed_sum)
+
+            n = self.cluster_size.sum()
+            cluster_size = (
+                (self.cluster_size + self.eps) / (n + self.n_emb * self.eps) * n
+            )
+            embed_normalized = self.embed_avg / cluster_size.unsqueeze(0)
+            self.weight.data.copy_(embed_normalized)
+
+        return result, argmin
